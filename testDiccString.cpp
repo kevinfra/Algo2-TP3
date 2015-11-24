7957bed576293013379e696cb6af32f144f12fca
axelnico <nicobuk@gmail.com>
2015-11-23 22:08:58 -0300

    funciona todo DiccString pero tiene leaks

--- testDiccString.cpp
+++ testDiccString.cpp
@@ -7,12 +7,14 @@ using namespace aed2;
 int main(int argc, char **argv){
 	DiccString<int> midic;
 	midic.Definir("ab",2);
-	//midic.Definir("ab",1);
-	//midic.Definir("a",2);
-	//midic.Definir("ccc",3);
+	midic.Definir("ab",1);
+	midic.Definir("ab",3);
 	//midic.Eliminar("ab");
 	//midic.Eliminar("a");
 	//midic.Eliminar("ccc");
-	//DiccString<int>::Iterador it =  midic.CrearIt();
-	
+	DiccString<int>::Iterador it =  midic.CrearIt();
+	while(it.HaySiguiente()){
+		cout << it.SiguienteSignificado() << endl;
+		it.Avanzar();
+	}
 }
